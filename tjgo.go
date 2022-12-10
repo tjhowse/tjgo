a71dd86c54046af9eb3bf698c40fc06eba967620
Travis Howse <tjhowse@gmail.com>
2022-12-10 20:49:17 +1000

    Add comments and regex splitting.

--- tjgo.go
+++ tjgo.go
@@ -32,6 +32,7 @@ func (v *Vec2) Scale(s float64) {
 	v.Y = int(float64(v.Y) * s)
 }
 
+// This splits a file into a slice, one line per element.
 func FileToSlice(filename string) []string {
 	var contents []string
 	file, err := os.Open(filename)
@@ -49,6 +50,7 @@ func FileToSlice(filename string) []string {
 	return contents
 }
 
+// This splits a file into a 2D slice of strings based on the provided delimiter.
 func FileTo2DSlice(filename string, delimiter rune) [][]string {
 	var contents [][]string
 	file, err := os.Open(filename)
@@ -59,6 +61,28 @@ func FileTo2DSlice(filename string, delimiter rune) [][]string {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		s := strings.Split(scanner.Text(), string(delimiter))
+		contents = append(contents, s)
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+	return contents
+}
+
+// This splits a file into a 2D slice of strings. Each line is processed
+// with the provided regex. Each capture group of the regex produces one
+// element of each slice in the result.
+func FileTo2DSliceRegex(filename string, regex string) [][]string {
+	var contents [][]string
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	rgx := regexp.MustCompile(regex)
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s := DoRegexFaster(scanner.Text(), rgx)
 		contents = append(contents, s)
 	}
 	if err := scanner.Err(); err != nil {
@@ -122,7 +146,11 @@ func Poop() string {
 
 func DoRegex(input, regex string) []string {
 	rgx := regexp.MustCompile(regex)
-	results := rgx.FindStringSubmatch(input)
+	return DoRegexFaster(input, rgx)
+}
+
+func DoRegexFaster(input string, regex *regexp.Regexp) []string {
+	results := regex.FindStringSubmatch(input)
 	if len(results) > 1 {
 		return results[1:]
 	}
